0a7940fcbfbe58927292e56aad2051f2433adbcb
kimjh11130 <kimjh11130@naver.com>
2024-07-17 12:14:54 +0900

    회원가입 페이지 퍼블리싱

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { Button, Input, Template } from '@/components';
+
+const SignupPage = () => {
+  return (
+    <Template>
+      <Container>
+        <Wrapper>
+          <Title>회원가입</Title>
+          <InputWrapper>
+            <Input label='아이디' />
+            <Input label='비밀번호' type='password' />
+            <Input label='이름' />
+            <Input label='현재 자산' unit='원' />
+          </InputWrapper>
+        </Wrapper>
+        <Button
+          guideMessage='이미 계정이 있으신가요?'
+          highlight='로그인하기'
+          link='login'
+        >
+          회원가입
+        </Button>
+      </Container>
+    </Template>
+  );
+};
+
+export default SignupPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
